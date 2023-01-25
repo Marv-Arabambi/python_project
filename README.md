ca046dc27c237d578b7a3836ce16ab639c657f17
Marvellous Arabambi <rt22543@bristol.ac.uk>
2023-01-25 03:59:07 +0000

    Added the notebook and the files plot.py, model.py and README.md

---
+++ README.md
@@ -0,0 +1,43 @@
+# KNN and PCA model development notebook
+## Created by Marvellous Arabambi (University of Bristol)
+This repository can be found at : https://github.com/Marv-Arabambi/python_project.git
+
+## Decription
+This notebook walks you through the process of using *K-Nearest Neighbors (KNN)* to develop a model which if fited to a training data set. Using the model, decision boundaries can be decided and prediction made on what class a sample falls. *Principle components analysis (PCA)* is conducted alongside the KNN inorder to take into consideration relationships between varaibles when developing the model. 
+
+Here we are working though a data set which include different features of tumors. We will develop a model and then make prediction on wheather our tumor is classed as either _benign_ or _malignant_ based on the measurements we input to the model. 
+
+There is scope with in the notebook to use *your own data set* to form a model and then makes some predictions. Your data set must be formated in a CSV file correctly. However, your columns should include thoese labled: "mean radius", "mean texture", "mean perimeter", "mean area", "mean smoothness", "mean compactness", "mean concavity", "mean concave points", "mean symmetry", "mean fractal dimension". The column representing the $y$ result should only contain values of either 0 (to represent benign diagnoses) or 1 (representing a malignant diagnoses)
+
+## Dependencies
+numpy
+
+matplotlib
+
+scikit-learn (1.2.0)
+
+## Installing
+The notebook to be downloaded is:
+
+KNN_and_PCA_model_development.ipynb
+
+The file that also must be downloaded that the script calls are:
+
+1. plot.py
+2. model.py
+
+(remember that these files should be in the same directory as the notebook)
+
+## Execting the Program
+
+Run through the script in the notbook using the markdown cells and annotations as guide
+
+## Reference
+data set was sourced from:
+@misc{Dua:2019 ,
+author = "Dua, Dheeru and Graff, Casey",
+year = "2017",
+title = "{UCI} Machine Learning Repository",
+url = "http://archive.ics.uci.edu/ml",
+institution = "University of California, Irvine, School of Information and Computer Sciences" }
+
---
+++ model.py
@@ -0,0 +1,32 @@
+# Create a function called optimise model which takes 4 argument: train_X_s, train_y_s, test_X_s, test_y_s. The training and testing data sets
+def optimise_model(train_X_s, train_y_s, test_X_s, test_y_s): 
+    import numpy as np                                                    # for working with arrays and DataFrames
+    import pandas as pd                                                   # for storing our DataFrames
+    from sklearn.decomposition import PCA                                 # to conduct the PCA
+    from sklearn.neighbors import KNeighborsClassifier                    # we would be using a K Neightbours model
+    from sklearn.preprocessing import StandardScaler                      # for rescaling each variable
+    from sklearn.pipeline import make_pipeline                            # to make a pipeline we can pass our data through
+    from sklearn.model_selection import GridSearchCV                      # for optimising the hyper-parameters
+
+    results = pd.DataFrame([])  # an empty DataFrame we can fill as we go through the loop below
+    
+    for i in range(1,51):      #for loop which will run the GridSearch with different number of PCA components and neighbour
+        model = GridSearchCV(
+            make_pipeline(
+                StandardScaler(),   #rescale inorder to take into account the different scales of the different x values
+                PCA(),
+                KNeighborsClassifier(n_neighbors=i) # loop through differing number of neighbour
+            ),
+            {
+                "pca__n_components" : range(1, 10), # GridSearch will loop through and give the optimum PCA 
+            }                                       # for the current value of i (number of neighbour)
+        )
+        model.fit(train_X_s, train_y_s)        # fit best model, with current i value, to training data
+        score = model.score(test_X_s, test_y_s)      #use test data to score the model
+        best_pca = model.best_estimator_["pca"].n_components_   #give value of the optimum PCA component value for 
+                                                                #the current number of neighbour tested
+        results = results.append(pd.DataFrame({"number of neighbours": i, "best PCA": best_pca, "score": score}, 
+                                              index= [0]), ignore_index=True)    # put all the results in a table 
+
+    return(results)   # function returns this table
+   
